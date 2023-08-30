78eb229723ab4193d0f61192ded0266eff659fb9
RobiulIqbal <qobel98@gmail.com>
2023-08-30 21:19:28 +0700

    cors

--- app.js
+++ app.js
@@ -24,27 +24,27 @@ const apiRouter = require("./routes/api");
 
 var app = express();
 
-
-
+app.use(cors());
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(methodOverride('_method'));
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 }
-}));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
 app.use(flash());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/sb-admin-2", express.static(path.join(__dirname, "node_modules/startbootstrap-sb-admin-2")));
 
-app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
